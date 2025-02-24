c985c314084edbcc27f67a1ac43536439cd1b2b3
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-02-24 17:09:13 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.vsce
+            pkgs.python3
+          ];
+        };
+      }
+    );
+}
